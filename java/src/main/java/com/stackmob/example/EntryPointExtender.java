57f8d24505ec886c7f440232ff4a0a2e0dd309d5
Natapong Thongkamkoon <natapong.th@gmail.com>
2013-03-25 16:52:52 +0700

    Add Delete events method

--- java/src/main/java/com/stackmob/example/EntryPointExtender.java
+++ java/src/main/java/com/stackmob/example/EntryPointExtender.java
@@ -29,14 +29,14 @@ public class EntryPointExtender extends JarEntryObject {
     List<CustomCodeMethod> list = new ArrayList<CustomCodeMethod>();
 	list.add(new InitializeUser());
 	list.add(new GetDatabase());
-	list.add(new CreateRelationships());
-	list.add(new CreateNewGroup());
 	list.add(new UpdateUser());
+	list.add(new CreateRelationships());
 	list.add(new UpdateRelationships());
+	list.add(new CreateNewGroup());
 	list.add(new UpdateGroup());
 	list.add(new DeleteGroup());
-	list.add(new DeleteEvent());
 	list.add(new CreateStatusRequest());
+	list.add(new DeleteEvents());
     return list;
   }
 
